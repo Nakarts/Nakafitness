94bdc0e0c71c6d8dcef4edda8a0a87ce00cf7643
Nakarts <gomistony@live.fr>
2026-04-19 11:36:42 +0200

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-const CACHE_NAME = "nakafitness-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+const CACHE = "nakafitness-v2";
+const ASSETS = ["./", "./index.html"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match("./index.html")))); });
